2783db2f760dd8681c0fb1cebbfc118d922779d5
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 19:16:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import json
+import ee
 import streamlit as st
+import os 
 dictionary = {'type':st.secrets['Google_Earth_Engine']['type'],
               'project_id':st.secrets['Google_Earth_Engine']['project_id'],
               'private_key_id':st.secrets['Google_Earth_Engine']['private_key_id'],
@@ -13,41 +15,10 @@ dictionary = {'type':st.secrets['Google_Earth_Engine']['type'],
              }
 
 
-jsonString = json.dumps(dictionary)
-st.write(jsonString)
-
-import os 
 PathtoKeyFile=os.path.join(os.getcwd(), "key.json")
 with open(PathtoKeyFile, 'w') as outfile:
     json.dump(dictionary, outfile)
 
-
-import ee
-import streamlit as st
-
-from google.oauth2 import service_account
-
-import os 
-st.write(os.environ)
-#st.write(os.listdir(os.getcwd()).streamlit )
-from google.oauth2 import service_account
-
-newfile=os.path.join(os.getcwd(), ".streamlit")
-st.write(os.listdir(newfile))
-
-newnewfile=os.path.join(newfile, "secrets.toml")
-
-st.write(os.getcwd()+"/.streamlit/secrets.toml")
-
-#st.write(st.secrets["gee_service_account"]["client_email"])
-#st.write(st.secrets["gee_service_account"])
-#credentials = service_account.Credentials.from_service_account_info(
-#    st.secrets["gee_service_account"]
-#)
-
-
-
-st.write(os.path.exists(newnewfile))
 EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['Google_Earth_Engine']['client_email'], PathtoKeyFile)
 ee.Initialize(EE_CREDENTIALS)
 
