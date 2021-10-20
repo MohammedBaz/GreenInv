b295c74653931d5b8d60f7eaf6098f156b4e94b2
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 15:30:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,4 @@ import streamlit as st
 from google.oauth2 import service_account
 
 # Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["Google_Earth_Engine"]
-)
-client = ee.Initialize(credentials=credentials)
-
-
-st.write("initalised")
+st.write("DB username:",st.secrets["type"])
