bf361fa69e3bbdbfe6aa0b53d0aa43e769ad0fbe
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 16:54:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 from google.oauth2 import service_account
 
 
-st.write(st.session_state)
+st.write(STREAMLIT_*)
 from google.oauth2 import service_account
 
 #st.write(st.secrets["gee_service_account"]["client_email"])
