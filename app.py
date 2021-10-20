92ac81893b42bb1986b1067c7e9546fa156afc18
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 16:47:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
+import streamlit as st
+from google.auth.transport.requests import AuthorizedSession
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_file(st.secrets["gee_service_account"])
+scoped_credentials = credentials.with_scopes(
+    ['https://www.googleapis.com/auth/cloud-platform'])
+
+session = AuthorizedSession(scoped_credentials)
+
+url = 'https://earthengine.googleapis.com/v1beta/projects/earthengine-public/assets/LANDSAT'
+
+response = session.get(url)
+
+from pprint import pprint
+import json
+st.write(json.loads(response.content))
+
+
 import ee
 import streamlit as st
 
