3d372e2d545da43e0095ef43a59c444fdb259e72
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 16:31:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ from google.oauth2 import service_account
 #st.write(st.secrets["gee_service_account"]["client_email"])
 #st.write(st.secrets["gee_service_account"])
 
-EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets["gee_service_account"]["client_email"], **st.secrets.gee_service_account)
+EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets["gee_service_account"]["client_email"], st.secrets["gee_service_account"])
 ee.Initialize(EE_CREDENTIALS)
