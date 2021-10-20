67580b2001ba4ab6894c127b09c7f70e4fa2162e
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 16:55:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from google.oauth2 import service_account
 
 st.write(STREAMLIT_*)
 from google.oauth2 import service_account
-
+streamlit config show
 #st.write(st.secrets["gee_service_account"]["client_email"])
 #st.write(st.secrets["gee_service_account"])
 #credentials = service_account.Credentials.from_service_account_info(
