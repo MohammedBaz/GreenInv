b4f572557e57777f9efd012454621a933e7b8b16
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 20:59:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,25 @@ with open(os.path.join(os.getcwd(), "key.json"), 'w') as outfile:
 import ee
 EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['client_email'], PathtoKeyFile)
 ee.Initialize(EE_CREDENTIALS)
+
+i_date = '2000-01-01' # Initial date of interest (inclusive).
+f_date = '2021-01-01'# Final date of interest (exclusive).
+
+lc = ee.ImageCollection('MODIS/006/MCD12Q1')# Import the MODIS land cover collection.
+lst = ee.ImageCollection('MODIS/006/MOD11A1')# Import the MODIS land surface temperature collection.
+elv = ee.Image('USGS/SRTMGL1_003')# Import the USGS ground elevation image.
+
+lst = lst.select('LST_Day_1km', 'QC_Day').filterDate(i_date, f_date)# Selection of appropriate bands and dates for LST.
+
+u_lat = 21.0807514
+u_lon = 40.2975893
+u_poi = ee.Geometry.Point(u_lon, u_lat)# Define the urban location of interest as a point near Lyon, France. 
+
+r_lon = 40.3173763
+r_lat = 21.3618329
+r_poi = ee.Geometry.Point(r_lon, r_lat) # Define the rural location of interest as a point away from the city.
+
+
+scale = 1000  # scale in meters
+elv_urban_point = elv.sample(u_poi, scale).first().get('elevation').getInfo()# Print the elevation near Lyon, France.
+st.write(elv_urban_point)
