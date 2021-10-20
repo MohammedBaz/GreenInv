0da401db6ea3a1f7c32b01437f4d10505c7d06ad
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 18:58:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ st.write(os.getcwd()+"/.streamlit/secrets.toml")
 #credentials = service_account.Credentials.from_service_account_info(
 #    st.secrets["gee_service_account"]
 #)
-import jetblack-tomlutils
-toml2json newnewfile- > newnewfile.json
+
+
 
 st.write(os.path.exists(newnewfile))
 EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['gee_service_account']['client_email'], newnewfile)
