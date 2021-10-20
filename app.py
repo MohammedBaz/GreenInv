92ebe4c526a694c648c24fa2933f2f455cb4bebc
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 15:36:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ import streamlit as st
 from google.oauth2 import service_account
 
 # Create API client.
-st.write("DB username:",st.secrets["type"])
+st.write("DB username:",st.secrets["client_email"])
+
+#EE_PRIVATE_KEY_FILE = 'privatekey.json'
+
+EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets["client_email"])
