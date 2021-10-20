df0cb6ebe9107bb3e1e249e3198edb9c743010f1
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 15:49:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from google.oauth2 import service_account
 
 #EE_PRIVATE_KEY_FILE = 'privatekey.json'
 
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gee_service_account"]
-)
-client = ee.ServiceAccountCredentials(credentials=credentials)
+#credentials = service_account.Credentials.from_service_account_info(
+#    st.secrets["gee_service_account"]
+#)
+#client = ee.ServiceAccountCredentials(credentials=credentials)
+EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets["gee_service_account"]["client_email"], st.secrets["gee_service_account"])
