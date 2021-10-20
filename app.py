4b1956ce02334d3606cf1abc231bc157c833ddf3
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 16:41:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ import streamlit as st
 
 from google.oauth2 import service_account
 
+
+
+from google.oauth2 import service_account
+
 #st.write(st.secrets["gee_service_account"]["client_email"])
 #st.write(st.secrets["gee_service_account"])
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gee_service_account"]
+)
 
-EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['gee_service_account']['client_email'], '.streamlit/secrets.toml')
+
+EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['gee_service_account']['client_email'], credentials)
 ee.Initialize(EE_CREDENTIALS)
