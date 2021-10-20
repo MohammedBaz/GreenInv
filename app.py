ccfafaa6a9410f2c4f9bd1971f0bd1d8f915555e
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 15:46:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import streamlit as st
 from google.oauth2 import service_account
 
 # Create API client.
-st.write("DB username:",st.secrets["client_email"])
 
 #EE_PRIVATE_KEY_FILE = 'privatekey.json'
 
