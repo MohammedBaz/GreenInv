7d483f854f65aa5510771ae8661d740ae964d318
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-20 17:22:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,6 @@ st.write(os.getcwd()+"/.streamlit/secrets.toml")
 #    st.secrets["gee_service_account"]
 #)
 
-
+os.path.exists(newnewfile)
 EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['gee_service_account']['client_email'], newnewfile)
 ee.Initialize(EE_CREDENTIALS)
