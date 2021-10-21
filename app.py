41840bdb976df06247f8eb52a4331f0a137516b1
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-21 19:01:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def GetInformtionFromGoogleEarth(ImageCollectionName,ListofBands,Resultion,Start
   results=FilteredImageCollections.getRegion(PoI, Resultion).getInfo() # get the time series of the required bands
   resultsdf=pandas.DataFrame(results) #Cast the results getten from the above to dataframe
   headers = resultsdf.iloc[0] # set the header of dataframe to the first line of the results
-  resultsdf = pd.DataFrame(resultsdf.values[1:], columns=headers) # assign the results to the dataframe and use headers as columns names
+  resultsdf = pandas.DataFrame(resultsdf.values[1:], columns=headers) # assign the results to the dataframe and use headers as columns names
   resultsdf = resultsdf.dropna() # drops all rows with no data 
   for band in ListofBands: # Convert the data to numeric values
         resultsdf[band] = pandas.to_numeric(resultsdf[band], errors='coerce')
