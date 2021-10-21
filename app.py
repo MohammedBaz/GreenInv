7e3f57c24372fb59b56ce9184ed2b008b4ff1de0
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-21 22:18:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -112,6 +112,13 @@ with st.sidebar.expander("Please select the dataset we wish to work on"):
     #PlotBandTimeSeries(results['datetime'], results[ListofBands])
     with SubMainPageDescription:
       PlotlyBandTimeSeries(results['datetime'], results[ListofBands],InputedBand)
+      
+import folium
+m = folium.Map(location=[Longitude, Latitude], zoom_start=16)
+tooltip = "Liberty Bell"
+folium.Marker([Longitude, Latitude], popup="Liberty Bell", tooltip=tooltip).add_to(m)
+folium.folium_static(m)
+      
 
 
 
