ace13083267252b20715b834aaaaa619abadc70e
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-21 23:26:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -128,6 +128,22 @@ def add_ee_layer(self, ee_image_object, vis_params, name):
 
 folium.Map.add_ee_layer = add_ee_layer
 
+image = ee.Image('LANDSAT/LC08/C01/T1_TOA/LC08_044034_20140318')
+
+# Define the visualization parameters.
+image_viz_params = {
+    'bands': ['B5', 'B4', 'B3'],
+    'min': 0,
+    'max': 0.5,
+    'gamma': [0.95, 1.1, 1]
+}
+
+# Define a map centered on San Francisco Bay.
+map_l8 = folium.Map(location=[37.5010, -122.1899], zoom_start=10)
+
+# Add the image layer to the map and display it.
+map_l8.add_ee_layer(image, image_viz_params, 'false color composite')
+display(map_l8)
 
 
 
