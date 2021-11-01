63e5a00da57960364de21bf889ac7eeacc538dca
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-11-02 00:44:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,7 @@ file_ = open("movie.gif", "rb")
 contents = file_.read()
 data_url = base64.b64encode(contents).decode("utf-8")
 file_.close()
-
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
+)
