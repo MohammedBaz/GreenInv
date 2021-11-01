64bb1b3dcecac7bf06d2ba4539ed61e75d9a3202
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-11-02 02:24:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,10 @@
-import json
-import streamlit as st
-import os 
-import ee
-
-dictionary = {'type':st.secrets['type'],
-              'project_id':st.secrets['project_id'],
-              'private_key_id':st.secrets['private_key_id'],
-              'private_key':st.secrets['private_key'],
-              'client_email':st.secrets['client_email'],
-              'client_id':st.secrets['client_id'],
-              'auth_uri':st.secrets['auth_uri'],
-              'token_uri':st.secrets['token_uri'],
-              'auth_provider_x509_cert_url':st.secrets['auth_provider_x509_cert_url'],
-              'client_x509_cert_url':st.secrets['client_x509_cert_url']
-             }
-PathtoKeyFile=os.path.join(os.getcwd(), "key.json") #https://signup.earthengine.google.com/#!/service_accounts
-with open(os.path.join(os.getcwd(), "key.json"), 'w') as outfile:
-  json.dump(dictionary, outfile)
-EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['client_email'], PathtoKeyFile)
-ee.Initialize(EE_CREDENTIALS)
-st.write("____________________________________ Initalised______________________________________________")
-
-from GetImageCollections import getImageCollectionbyCoords,TemperatureCorrectionandConversionto,getImageCollectionbyCountry,egetImageCollectionbyCountry
-
-from PlottingFuncions import plotTimeSeries1
-import pandas
-import streamlit as st
-
-import matplotlib.pyplot as plt
-def PlotBandTimeSeries(TimeSeries,ValueofBand):     
-  fig, ax = plt.subplots()
-  plt.plot(TimeSeries, ValueofBand)
-  st.pyplot(fig)
-  
-def PlotlyBandTimeSeries(TimeSeries,ValueofBand,yaxesTitle):
-      import plotly.express as px
-      fig = px.line(results, x='datetime', y=ListofBands)
-      fig.update_xaxes(title_text='Time')
-      fig.update_yaxes(title_text=yaxesTitle)
-      st.plotly_chart(fig)
-    
+import streamlit as st   
  
 #############################################################Read the datasets#################################################################
 
 BandInformation=pandas.read_csv('BandInformation.csv',parse_dates=['StartDate', 'EndDate'])
 
 #############################################################Page Layout starts here############################################################
-
-#st.set_page_config(layout="wide") just change the page to wide mode
-#st.markdown(""" <style> #MainMenu {visibility: hidden;} footer {visibility: hidden;} </style> """, unsafe_allow_html=True)
 
 st.title("This is a POC app dedicated to Green initiative")
 st.header("                                                   ")
@@ -60,8 +16,8 @@ Sub3MainPageDescription=st.empty() # same as above
 
 import datetime
 with st.sidebar.expander("Please select the dataset we wish to work on"):
-  InputedBand = st.selectbox('Please select the meteorological dataset',BandInformation['ArabicDescription'])
-
+  InputedBand = st.selectbox('Please select the meteorological dataset',['مؤشر الحالة النباتية '])
+"""
   if InputedBand is not None:
     RowIndex=BandInformation[BandInformation['Description']==InputedBand].index[0]
     
@@ -130,4 +86,4 @@ def plotIndictors(aProvince,df):
   st.plotly_chart(fig)
 
 plotIndictors('Asir',VegetationConditionIndex)
-
+"""
