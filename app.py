6425c39a08dac4e76682755f865a208d4cd3c41a
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-11-05 01:01:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -76,10 +76,12 @@ with st.sidebar.expander('الرجاء اختيار المؤشر'):
     EndDate=BandInformation['EndDate'][RowIndex]
     TimeSelector = st.date_input("Pick a date", (StartDate, EndDate))
     
-    egetImageCollectionbyCountry(CountryName=['Saudi Arabia'],
-                                 ImageCollectionName=ImageCollectionName,
-                                 BandName=ListofBands,
-                                 StartDate=TimeSelector[0],
+    st.write("The strating date is:",TimeSelector[0])
+    st.write("The end date is",TimeSelector[1])
+    #egetImageCollectionbyCountry(CountryName=['Saudi Arabia'],
+    #                             ImageCollectionName=ImageCollectionName,
+    #                             BandName=ListofBands,
+    #                             StartDate=TimeSelector[0],
                                  EndDate=TimeSelector[1])
 ##############################
 
