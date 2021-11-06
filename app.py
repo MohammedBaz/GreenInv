16461e440eb91086c5c93993da328443d00b2b88
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-11-07 02:08:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ SubMainPageDescription=st.empty() # subcanvas where the inputs/outputs are handl
 Sub2MainPageDescription=st.empty() # same as above
 Sub3MainPageDescription=st.empty() # same as above
 #####################ArabicDescription=Description
-st.sidebar.write("This app uses real time data obtained from satellite imagery datasets to monitor several climate, Air Quality and Land Cover's parameters of KSA, beta version.")
+st.sidebar.write("This app uses real time data obtained from satellite imagery datasets to monitor several climate, Air Quality and Land Cover's parameters of KSA, beta version.Your comments and suggestions would be greatly appreciated at mdbaz01@gmail.com")
 with st.sidebar.expander('Please select Parameter'):
   InputedBand= st.radio('',BandInformation['Description'])  
   
