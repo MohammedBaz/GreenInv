b6897374f11a6e186b3efc98a884dbbd3da2a4a3
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-11-07 01:40:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ with st.sidebar.expander('Please select Parameter'):
     with MainPageDescription:
       #st.image(results[1],use_column_width=True,caption='Image of'+ListofBands)
       image = Image.open(ListofBands+'.gif')
-      st.image(image,use_column_width=True,caption='Image of'+ Description)
+      st.image(image,use_column_width=True,caption='Image of'+ InputedBand)
     with SubMainPageDescription:
       plotIndictors(ListofBands,results[0])
     with Sub2MainPageDescription:
