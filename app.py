678f3f0b1fb5667df77c4c66c76f2e9f8a79f6b6
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-11-07 01:56:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -104,9 +104,9 @@ with st.sidebar.expander('Please select Parameter'):
                                         )
     
     with MainPageDescription:
-      st.image(results[1],use_column_width=True,caption='Image of '+ InputedBand+ ' for the Interval from '+StartDate+' until '+EndDate)
+      st.image(results[1],use_column_width=True,caption='Image of '+ InputedBand+ ' for the Interval from '+str(StartDate)+' until '+ str(EndDate))
       #image = Image.open(ListofBands+'.gif')
-      #st.image(image,use_column_width=True,caption='Image of'+ InputedBand+ 'for the Interval from'+StartDate+'until'+EndDate)
+      #st.image(image,use_column_width=True,caption='Image of'+ InputedBand+ 'for the Interval from'+ str(StartDate)+'until'+EndDate)
     with SubMainPageDescription:
       plotIndictors(ListofBands,results[0])
     with Sub2MainPageDescription:
