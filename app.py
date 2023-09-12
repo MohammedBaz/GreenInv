2e288002435986737b2c20ae367556af047ea0f2
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2023-09-12 19:40:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import json
 import streamlit as st
 import os 
+import ee
+
 dictionary = {'type':st.secrets['type'],
               'project_id':st.secrets['project_id'],
               'private_key_id':st.secrets['private_key_id'],
@@ -12,42 +14,18 @@ dictionary = {'type':st.secrets['type'],
               'auth_provider_x509_cert_url':st.secrets['auth_provider_x509_cert_url'],
               'client_x509_cert_url':st.secrets['client_x509_cert_url']
              }
-
-#https://signup.earthengine.google.com/#!/service_accounts
-PathtoKeyFile=os.path.join(os.getcwd(), "key.json")
+PathtoKeyFile=os.path.join(os.getcwd(), "key.json") #https://signup.earthengine.google.com/#!/service_accounts
 with open(os.path.join(os.getcwd(), "key.json"), 'w') as outfile:
-    json.dump(dictionary, outfile)
-
-import ee
+  json.dump(dictionary, outfile)
 EE_CREDENTIALS = ee.ServiceAccountCredentials(st.secrets['client_email'], PathtoKeyFile)
 ee.Initialize(EE_CREDENTIALS)
 st.write("____________________________________ Initalised______________________________________________")
+
+from GetImageCollections import getImageCollectionbyCoords,TemperatureCorrectionandConversionto,getImageCollectionbyCountry,egetImageCollectionbyCountry
+
+from PlottingFuncions import plotTimeSeries1
 import pandas
-
-def GetInformtionFromGoogleEarth(ImageCollectionName,ListofBands,Resultion,StartDate,EndDate,Latitude,Longitude):
-  PoI = ee.Geometry.Point(Longitude, Latitude) # Cast Lat and Long into required class
-  ImageCollection=ee.ImageCollection(ImageCollectionName) # get the image collecton from google earthengine
-  FilteredImageCollections = ImageCollection.select(ListofBands).filterDate(StartDate, EndDate) # apply filter(s):time and/or bands
-  results=FilteredImageCollections.getRegion(PoI, Resultion).getInfo() # get the time series of the required bands
-  resultsdf=pandas.DataFrame(results) #Cast the results getten from the above to dataframe
-  headers = resultsdf.iloc[0] # set the header of dataframe to the first line of the results
-  resultsdf = pandas.DataFrame(resultsdf.values[1:], columns=headers) # assign the results to the dataframe and use headers as columns names
-  resultsdf = resultsdf.dropna() # drops all rows with no data 
-  for band in ListofBands: # Convert the data to numeric values
-        resultsdf[band] = pandas.to_numeric(resultsdf[band], errors='coerce')
-  resultsdf['datetime'] = pandas.to_datetime(resultsdf['time'], unit='ms') # Convert the time field into a datetime.
-  resultsdf = resultsdf[['time','datetime',  *ListofBands]]
-  return resultsdf
-
-def TemperatureCorrectionandConversionto(RawData):
-   #This funcion is applied to LST_Day_1km bands of MODIS/006/MOD11A1 image collection as
-   # the returned readings need to multiplied by correction factor and converted to Celsius
-   # here we ignore the QC_Day but it need to be consider at production time, good source can be
-   # found at https://spatialthoughts.com/2021/08/19/qa-bands-bitmasks-gee/
-   requiredresults =  0.02*RawData - 273.15
-   return requiredresults
-
-
+import streamlit as st
 
 import matplotlib.pyplot as plt
 def PlotBandTimeSeries(TimeSeries,ValueofBand):     
@@ -98,7 +76,7 @@ with st.sidebar.expander("Please select the dataset we wish to work on"):
     #TimeSelector = st.date_input("Pick a date", (StartDate, EndDate))
     #st.write("The strating date is:",TimeSelector[0])
     #st.write("The end date is",TimeSelector[1])
-    results=GetInformtionFromGoogleEarth(ImageCollectionName=ImageCollectionName,
+    results=getImageCollectionbyCoords(ImageCollectionName=ImageCollectionName,
                                      ListofBands=[ListofBands],
                                      Resultion=int(Resultion),
                                      StartDate=StartDate,
@@ -112,53 +90,43 @@ with st.sidebar.expander("Please select the dataset we wish to work on"):
     #PlotBandTimeSeries(results['datetime'], results[ListofBands])
     with SubMainPageDescription:
       PlotlyBandTimeSeries(results['datetime'], results[ListofBands],InputedBand)
-      
-import folium
-import streamlit_folium
-m = folium.Map(location=[Latitude,Longitude], zoom_start=16)
-def add_ee_layer(self, ee_image_object, vis_params, name):
-  map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
-  folium.raster_layers.TileLayer(
-      tiles=map_id_dict['tile_fetcher'].url_format,
-      attr='Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
-      name=name,
-      overlay=True,
-      control=True
-  ).add_to(self)
+################################################################
+import altair as alt
+results=getImageCollectionbyCountry(['Saudi Arabia'],'MODIS/006/MOD13A2','NDVI','2010-01-01','2020-2-1')
+plotTimeSeries1(results,'NDVI')
+st.altair_chart(plotTimeSeries1(results,'NDVI'))
+#####################################
+from PIL import Image
+eresults=egetImageCollectionbyCountry(['Saudi Arabia'],'MODIS/006/MOD13A2','NDVI','2010-01-01','2020-2-1')
+#image = Image.open(eresults[1])
+st.image(eresults[1], caption='Sunrise by the mountains')
 
-folium.Map.add_ee_layer = add_ee_layer
-
-image = ee.Image('LANDSAT/LC08/C01/T1_TOA/LC08_044034_20140318')
-# Define the visualization parameters.
-image_viz_params = {
-    'bands': ['B5', 'B4', 'B3'],
-    'min': 0,
-    'max': 0.5,
-    'gamma': [0.95, 1.1, 1]
-}
-
-# Define a map centered on San Francisco Bay.
-map_l8 = folium.Map(location=[Latitude,Longitude], zoom_start=10)
-
-# Add the image layer to the map and display it.
-map_l8.add_ee_layer(image, image_viz_params, 'false color composite')
-st.map(map_l8)
+#######################################
+from PIL import Image
+image = Image.open('movie.gif')
+st.image(image, caption='Sunrise by the mountains')
+##############################
+import streamlit as st
+import base64
 
 
-
-
-""""
-tile_layer = folium.TileLayer(
-    tiles="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png",
-    attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    max_zoom=19,
-    name='darkmatter',
-    control=False,
-    opacity=0.7
+file_ = open("movie.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
 )
-tile_layer.add_to(m)
-"""
+###############################################
+import plotly.express as px
+VegetationConditionIndex=pandas.read_csv('VegetationConditionIndex.csv')
+Provinces=VegetationConditionIndex['Province'].unique()
+def plotIndictors(aProvince,df):
+  filteredVegetationConditionIndex=df[df["Province"]==aProvince]
+  fig = px.line(filteredVegetationConditionIndex, x='Date', y='Data')
+  fig.update_xaxes(title_text='Time')
+  fig.update_yaxes(title_text='Vegetation Condition Index of'+aProvince)
+  st.plotly_chart(fig)
 
-streamlit_folium.folium_static(m)
-
- 
+plotIndictors('Asir',VegetationConditionIndex)
